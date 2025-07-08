66a0c68da50d394851e3ef86810dcd8a4e762406
Muaaz Saif <muaaz.saif@contour-software.com>
2025-07-08 12:58:10 +0500

    router.register was missing

--- django_candidate_profiles/Profile_builder_DB/urls.py
+++ django_candidate_profiles/Profile_builder_DB/urls.py
@@ -2,10 +2,6 @@
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
-from rest_framework.routers import DefaultRouter
-
-router = DefaultRouter()
-router.regist
 
 urlpatterns = [
     path('admin/', admin.site.urls),
