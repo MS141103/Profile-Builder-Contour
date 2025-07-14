899743d4aea28d1d780e284c9e7abe41e0221c25
Hamdia Nouman <130587490+HamdiaNouman-22@users.noreply.github.com>
2025-07-14 11:05:20 +0500

    Delete Profile_Builder directory

--- Profile_Builder/employees/utils/user_utils.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.models import User
-
-def create_user_account(user_data):
-    return User.objects.create_user(
-        username=user_data['username'],
-        email=user_data['email'],
-        password=user_data['password']
-    )
